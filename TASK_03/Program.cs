e94dd53df0f3dcee16eac8fc97920b363ac028ea
Alina Lavrik <alina_voro@mail.ru>
2022-09-22 21:37:11 +0300

    Add Task 03 квадрат чисел от 1 до N

---
+++ TASK_03/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+5 -> 1, 4, 9, 16, 25.
+
+2 -> 1,4
+*/ 
+
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int index = 1; index <= number; index ++)
+{
+    double result = Math.Pow(index, 2);
+    Console.Write($"{result} ");
+}
+
