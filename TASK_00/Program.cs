c1dfd5d4db57d84dcfd077521194d9233a198941
Alina Lavrik <alina_voro@mail.ru>
2022-09-22 22:18:19 +0300

    Add task 00

---
+++ TASK_00/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти
+
+
+Console.WriteLine("Введи четверть: ");
+int number = int.Parse(Console.ReadLine());
+
+switch(number){
+    case 1:
+    {
+       Console.WriteLine("Диапазон возможных значений четверти x > 0, y > 0");
+       break;
+    }
+    case 2:
+    {
+       Console.WriteLine("Диапазон возможных значений четверти x < 0, y > 0");
+       break;
+    }
+    case 3:
+    {
+       Console.WriteLine("Диапазон возможных значений четверти x < 0, y < 0");
+       break;
+    }
+    case 4:
+    {
+       Console.WriteLine("Диапазон возможных значений четверти x > 0, y < 0");
+       break;
+    }
+}
