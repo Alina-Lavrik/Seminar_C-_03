f26149f99c0e44ca492804715e503a5bd7eb464f
Alina Lavrik <alina_voro@mail.ru>
2022-09-22 21:58:09 +0300

    Add Task 02 расстояние между точками в 2D пространстве

---
+++ TASK_02/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+A (3,6); B (2,1) -> 5,09
+A (7,-5); B (1,-1) -> 7,21 */
+
+Console.Write("Введите координаты точки X: ");
+int X = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты точки Y: ");
+int Y = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты точки X1: ");
+int X1 = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты точки Y1: ");
+int Y1 = int.Parse(Console.ReadLine());
+
+double dist = Math.Sqrt(Math.Pow(X1 - X, 2) + Math.Pow(Y1 - Y, 2));  // Math.Pow - возведение в квадрат
+Console.Write($"Расстояние между двумя точками на плоскости равно {dist:f3}"); // :f3 - количество цифр после запятой
